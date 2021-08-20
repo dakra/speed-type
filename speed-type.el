3cec9fe34bc2bd83d43c3a69e937e2126031eb9f
Manolito <manolito@local>
2021-08-21 00:07:04 +0200

    Add custom default face
    A custom default face enable the user to set different face for
    speed-type.

--- speed-type.el
+++ speed-type.el
@@ -91,6 +91,11 @@ E.g. if you always want lowercase words, set:
   :type '(choice (const :tag "None" nil)
                  (symbol :tag "Language")))
 
+(defface speed-type-default
+  '()
+  "Default face for `speed-type'."
+  :group 'speed-type)
+
 (defface speed-type-correct
   '((t :foreground "green"))
   "Face for correctly typed characters."
@@ -390,6 +395,7 @@ language symbol and N-WORDS is the top N words that should be trained."
         (len (length text)))
     (set-buffer buf)
     (speed-type-mode)
+    (buffer-face-set 'speed-type-default)
     (setq speed-type--orig-text text)
     (setq speed-type--mod-str (make-string len 0))
     (setq speed-type--remaining len)
